9ea54a18967401090aa80ce871bb366e2588f744
sudiptoNayek00 <sudipto.nayek777@gmail.com>
2025-07-02 21:19:40 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 
 //placing user model in frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-del-app-gray.vercel.app/";
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   try {
     const newOrder = new orderModel({
